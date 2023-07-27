100fbcab9908bb8d9d902e91fb2560a799f46c5c
KaiO277 <nghiatran1527@gmail.com>
2023-07-27 10:44:06 +0700

    KaiO

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -3,14 +3,7 @@ const Course = require('../models/Course');
 class SiteController{
 
      //GET /ns
-     async index(req, res, next){
-            // try {
-            //   const courses = await Course.find({}).exec();
-            //   res.json(courses);
-            //   console.log(res.json(courses));
-            // } catch (err) {
-            //   next(err);
-            // }
+     async index(req, res, next){          
       Course.find({})
         .then(courses => {
           courses = courses.map(course => course.toObject())
